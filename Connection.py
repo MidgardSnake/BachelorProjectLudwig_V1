a826c6ea5c0d56609ce6174ed5765eff223cf7b2
MidgardSerpent <l.voss@campus.tu-berlin.de>
2024-02-27 13:01:29 +0100

    new connections

---
+++ Connection.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+def connect_to_database():
+    try:
+        connection = psycopg2.connect(
+            dbname="IMDb_Title",
+            user="postgres",
+            password="DKBLV1993",
+            host="localhost",
+            port="5432"
+        )
+        print("Verbindung zur Datenbank hergestellt.")
+
+        return connection
+    except Exception as e:
+        print("Fehler beim Herstellen der Verbindung zur Datenbank:", e)
+        return None
+
+#valid_tablenames: akas , basics, crew, episode , principals, ratings
+def select_from_table(connection, table_name):
+    try:
+        cursor = connection.cursor()
+        cursor.execute(f"SELECT * FROM {table_name}")
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except Exception as e:
+        print("Fehler beim Ausführen der Abfrage:", e)
+
+# Verbindung zur Datenbank herstellen,
+# Outprint von AKAS-Tabelle ist auskommentiert, Outprint dauert ewig, ist aber keine Endlosloop
+
+if __name__ == '__main__':
+    db_connection = ()
+
+#    if db_connection is not None:
+#        select_from_table(db_connection, "akas")
+
